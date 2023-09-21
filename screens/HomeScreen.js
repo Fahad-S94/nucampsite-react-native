64f3ffb07e7ad1f36b7b52fb44d47f09005ec786
Fahad Shoaib <f.shoaib1580@gmail.com>
2023-09-21 04:06:17 -0400

    Navigation Part 2

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+import { PROMOTIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
+
+const HomeScreen = () => {
+  return <Text></Text>;
+};
+
+export default HomeScreen;
