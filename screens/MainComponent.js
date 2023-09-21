b49262d5b6e4aed18707176a5a13e72a0088aca5
Fahad Shoaib <f.shoaib1580@gmail.com>
2023-09-21 01:40:24 -0400

    Components PArt 2

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,9 +1,31 @@
 import { useState } from 'react';
 import { CAMPSITES } from '../shared/campsites';
 import DirectoryScreen from './DirectoryScreen';
+import { View } from 'react-native';
+import CampsiteInfoScreen from './CampsiteInfoScreen';
+import DirectoryScreen from './DirectoryScreen';
 
 const Main = () => {
-  return <DirectoryScreen campsites={campsites} />;
+  const [campsites, setCampsites] = useState(CAMPSITES);
+  const [selectedCampsiteId, setSelectedCampsiteId] = useState();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <DirectoryScreen
+        campsites={campsites}
+        onPress={(campsiteId) => setSelectedCampsiteId(campsiteId)}
+      />
+      <CampsiteInfoScreen
+        campsite={
+          campsites.filter((campsite) => campsite.id === selectedCampsiteId)[0]
+        }
+      />
+    </View>
+  );
 };
 
 export default Main;
