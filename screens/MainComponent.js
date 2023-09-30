c52acbfec509dae15af4e1e5d5ecd6579b6b9365
Fahad Shoaib <f.shoaib1580@gmail.com>
2023-09-30 15:17:17 -0400

    Secure Store

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -13,6 +13,7 @@ import HomeScreen from './HomeScreen';
 import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
 import ReservationScreen from './ReservationScreen';
+import LoginScreen from './LoginScreen';
 
 import { Icon } from 'react-native-elements';
 import logo from '../assets/images/logo.png';
@@ -29,6 +30,28 @@ const Drawer = createDrawerNavigator();
 const screenOptions = {
   headerTintColor: '#fff',
   headerStyle: { backgroundColor: '#5637DD' },
+};
+
+const LoginNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={({ navigation }) => ({
+          headerLeft: () => {
+            <Icon
+              name="sign-in"
+              type="font-awesome"
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />;
+          },
+        })}
+      />
+    </Stack.Navigator>
+  );
 };
 
 const AboutNavigator = () => {
@@ -188,6 +211,21 @@ const Main = () => {
         drawerContent={CustomDrawerContent}
         drawerStyle={{ backgroundColor: '#CEC8FF' }}
       >
+        <Drawer.Screen
+          name="Login"
+          component={LoginNavigator}
+          options={{
+            drawerIcon: ({ color }) => (
+              <Icon
+                name="sign-in"
+                type="font-awesome"
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            ),
+          }}
+        />
         <Drawer.Screen
           name="Home"
           component={HomeNavigator}
