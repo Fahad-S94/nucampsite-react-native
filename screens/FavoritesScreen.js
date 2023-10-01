83a0a19f9f3bf14b0f6029e92fa7b161f6dc1516
Fahad Shoaib <f.shoaib1580@gmail.com>
2023-10-01 14:10:34 -0400

    Worhshop: W3

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,5 +1,3 @@
-import { useSelector } from 'react-redux';
-import { View, FlatList, Text } from 'react-native';
 import { Avatar, ListItem } from 'react-native-elements';
 import Loading from '../components/LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
